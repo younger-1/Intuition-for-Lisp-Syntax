2a4c1be920b27e74c44e82b89e2b0990e20e2ec6
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-01-21 14:17:27 +0800

    further simplification

--- app.js
+++ app.js
@@ -9,22 +9,25 @@
 // rotate(shape, 90)
 
 const fns = {
+    // normal
     drawPoint: (x, color) => console.log(x, color),
     drawLine: (x, y, color) => console.log(x, y, color),
     drawCircle: (x, r, color) => console.log(x, r, color),
     rotate: (s, pi) => console.log(s, pi),
+    // special
+    do: (...args) => args[args.length - 1],
 }
 
 // user data
 
-data = {
-    instructions: [
-        ["drawPoint", { x: 0, y: 0 }, "blue"],
-        ["drawPoint", { x: 1, y: 1 }, "blue"],
-        // rotate(drawLine({ x: 0, y: 0 }, { x: 1, y: 1 }, 'yellow'), 90)
-        ["rotate", ["drawLine", { x: 0, y: 0 }, { x: 1, y: 1 }, 'yellow'], 90],
-    ]
-}
+data = [
+    "do",
+    ["drawPoint", { x: 0, y: 0 }, "blue"],
+    ["drawPoint", { x: 1, y: 1 }, "blue"],
+    // rotate(drawLine({ x: 0, y: 0 }, { x: 1, y: 1 }, 'yellow'), 90)
+    ["rotate", ["drawLine", { x: 0, y: 0 }, { x: 1, y: 1 }, 'yellow'], 90],
+]
+
 
 // main
 const parseInstruction = (ins) => {
@@ -35,7 +38,7 @@ const parseInstruction = (ins) => {
     const [fName, ...args] = ins;
     return fns[fName](...args.map(parseInstruction));
 };
-data.instructions.forEach(parseInstruction);
+parseInstruction(data);
 
 // output
 /*
