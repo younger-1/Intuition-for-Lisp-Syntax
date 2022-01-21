aa3ce9de9a0600911719a6f6ed951e22138c158d
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-01-21 15:59:44 +0800

    define functions

--- app.js
+++ app.js
@@ -28,18 +28,44 @@ const fns = {
 // user data
 data = [
     "do",
-    // const p1 = drawPoint({ x: 0, y: 0 }, "blue")
-    // const p2 = drawPoint({ x: 1, y: 1 }, "blue")
-    // const myline = drawLine(p1, p2, "yellow")
     ["def", "p1", ["drawPoint", { x: 0, y: 0 }, "blue"]],
     ["def", "p2", ["drawPoint", { x: 1, y: 1 }, "blue"]],
-    ["def", "myline", ["drawLine", "p1", "p2", "yellow"]],
-    ["rotate", "myline", 90],
+    ["def", "p3", ["drawPoint", { x: 1, y: 0 }, "blue"]],
+    ["rotate", ["drawLine", "p1", "p2", "yellow"], 90],
+    // const drawTriangle = function (left, top, right, color) {
+    //     drawLine(left, top, color);
+    //     drawLine(top, right, color);
+    //     drawLine(left, right, color);
+    // }
+    ["def", "drawTriangle",
+        ["fn", ["left", "top", "right", "color"],
+            ["do",
+                ["drawLine", "left", "top", "color"],
+                ["drawLine", "top", "right", "color"],
+                ["drawLine", "left", "right", "color"],
+            ],
+        ],
+    ],
+    // drawTriangle(p1, p2, p3, "yellow")
+    ["drawTriangle", "p1", "p2", "p3", "yellow"],
 ]
 
 
 // main
-const parseInstruction = (ins) => {
+// 将形参映射到实参：map parameters to arguments
+const mapArgsWithValues = (args, values) => {
+    return args.reduce((res, k, idx) => {
+        res[k] = values[idx];
+        return res;
+    }, {});
+}
+const parseFnInstruction = (args, body, oldVariables) => {
+    return (...values) => {
+        const newVariables = { ...oldVariables, ...mapArgsWithValues(args, values) };
+        return parseInstruction(body, newVariables);
+    };
+}
+const parseInstruction = (ins, variables) => {
     if (variables[ins]) {
         return variables[ins];
     }
@@ -48,9 +74,13 @@ const parseInstruction = (ins) => {
         return ins;
     }
     const [fName, ...args] = ins;
-    return fns[fName](...args.map(parseInstruction));
+    if (fName == "fn") {
+        return parseFnInstruction(...args, variables);
+    }
+    const fn = fns[fName] || variables[fName];
+    return fn(...args.map((arg) => parseInstruction(arg, variables)));
 };
-parseInstruction(data);
+parseInstruction(data, variables);
 
 // output
 /*
